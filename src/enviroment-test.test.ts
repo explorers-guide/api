625ca2314ef4bb14fbf1823158ce8337ccac704d
Daniel Barreto <munizbarreto5@gmail.com>
2021-12-08 20:35:31 -0300

    chore: add jest

---
+++ src/enviroment-test.test.ts
@@ -0,0 +1,6 @@
+import Circle from "./enviroment-test";
+
+test("Should calculate the area of circle", function () {
+    const circle = new Circle(2);
+    expect(circle.getArea()).toBe(12.566370614359172);
+});
